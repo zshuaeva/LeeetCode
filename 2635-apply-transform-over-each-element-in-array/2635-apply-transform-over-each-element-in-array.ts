a06e55d33cb250d292790462f910ce567426cf80
zshuaeva <114031451+zshuaeva@users.noreply.github.com>
2023-08-10 11:01:46 -0700

    Time: 62 ms (51.05%), Space: 42.8 MB (56.21%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
@@ -1,6 +1,5 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
     let result = []
-    console.log(fn)
     for (let i = 0; i < arr.length; i++) {
         result.push(fn(arr[i], i))
     }
