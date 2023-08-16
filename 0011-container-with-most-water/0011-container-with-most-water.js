8c10832200fc67a0145db7d579ac5b12b483a985
zshuaeva <114031451+zshuaeva@users.noreply.github.com>
2023-08-16 12:53:01 -0700

    Time: 54 ms (98.44%), Space: 49.9 MB (25.26%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+
+    while (left < right) {
+        const width = right - left;
+        const containerHeight = Math.min(height[left], height[right]);
+        const area = width * containerHeight;
+        maxArea = Math.max(maxArea, area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+};
