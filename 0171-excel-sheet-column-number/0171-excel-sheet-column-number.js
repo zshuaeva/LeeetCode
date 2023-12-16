dbf5e691d970eb0891e04a45c54f6a516d4b7383
zshuaeva <114031451+zshuaeva@users.noreply.github.com>
2023-12-15 16:19:30 -0800

    Time: 66 ms (30.19%), Space: 44.2 MB (17.69%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+    let result = 0;
+    
+    for (let i = 0; i < columnTitle.length; i++) {
+        const charValue = columnTitle.charCodeAt(i) - 'A'.charCodeAt(0) + 1;
+        result = result * 26 + charValue;
+    }
+    
+    return result;
+};
