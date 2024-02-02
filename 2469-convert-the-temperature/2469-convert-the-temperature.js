8c5f6c2bc06d6bb20cd8da40cc0208588e8aeecc
zshuaeva <114031451+zshuaeva@users.noreply.github.com>
2024-02-02 13:56:44 -0800

    Time: 39 ms (98.18%), Space: 48.6 MB (13.69%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.js
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -3,7 +3,5 @@
  * @return {number[]}
  */
 var convertTemperature = function(celsius) {
-    let kelv = celsius + 273.15
-    let fahr = celsius * 1.80 + 32.00
-    return([kelv, fahr])
+    return([(celsius + 273.15), (celsius * 1.80 + 32.00)])
 };
