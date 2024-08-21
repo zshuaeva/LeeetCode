91867decebc706d4ec693fc7004b4a5e0604df85
zshuaeva <114031451+zshuaeva@users.noreply.github.com>
2024-08-21 10:16:27 -0700

    Time: 71 ms (7.28%), Space: 52.2 MB (44.79%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,22 +3,19 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
+    const result = [];
 
-  var result = [];
-
-  for (var i = 1; i <= n; i++) {
-   
-    if (i % 3 === 0 && i % 5 === 0) {
-      result.push('FizzBuzz');
-    } else if (i % 3 === 0) {
-        result.push('Fizz');
-      } else if (i % 5 === 0) {
-          result.push('Buzz');
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
         } else {
-          result.push(i.toString());
-          }
-  }
-  return result;
-}
-  
- 
+            result.push(i.toString());
+        }
+    }
+
+    return result;
+};
